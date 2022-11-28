36fe68d9b584a55e3f886f8976601adce771db08
jshahadat <jshahadat8@gmail.com>
2022-11-28 22:58:58 +0600

    finle push

--- src/pages/Authintication/Register/Register.js
+++ src/pages/Authintication/Register/Register.js
@@ -62,7 +62,7 @@ const Register = () => {
 
                 <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
                     <div className="md:block hidden w-1/2">
-                        <img className="rounded-2xl" alt='' src="https://images.unsplash.com/photo-1616606103915-dea7be788566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80" />
+                        <img className="rounded-2xl" alt='' src="https://i.ibb.co/Dpb20Kv/welcome.webp" />
                     </div>
 
                     <div className="md:w-1/2 px-8 md:px-16">
